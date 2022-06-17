9f6fae7785f5de1eb373488a25164a57abd2ef0f
kajol-shrivastava <shrivastavkajol4285@gmail.com>
2022-06-17 23:37:17 +0530

    assignment completed

--- src/controllers/whetherController.js
+++ src/controllers/whetherController.js
@@ -1,10 +1,30 @@
 const axios=require("axios")
+//assignment 2
+let getWheather = async function (req, res) {
+    try {
+        let q= req.query.q
+        let appid=req.query.appid
+        let options = {
+            method: "get",
+            url:`http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appid}`
+        }
+        let result = await axios(options);
+       // console.log(result.data)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 
-let getWhether=async function(req,res){
+
+let getSortedTemp=async function(req,res){
   try {let citytemp=[]
     let appid=req.query.appid
     let cities= ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
-    for (let i in cities){console.log(cities[i])}
+    //for (let i in cities){console.log(cities[i])}
     for(let x in cities){
     let q=cities[x]
      let options={
@@ -12,16 +32,15 @@ let getWhether=async function(req,res){
         url:`http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appid}`
     }  
     let results=await axios(options)
-    console.log(results.data)
+    //console.log(results.data)
     let obj={city:q}
     obj.temp=results.data.main.temp
     citytemp.push(obj)
     }
-for(let i in citytemp){console.log(citytemp[i])}
+//for(let i in citytemp){console.log(citytemp[i])}
 let sortedcities=citytemp.sort(function(a,b){return a.temp-b.temp})
-
-    let data=sortedcities
-    res.status(200).send({status:true,msg:data})
+let data=sortedcities
+res.status(200).send({status:true,msg:data})
 }
 catch (err) {
     console.log(err)
@@ -30,4 +49,5 @@ catch (err) {
 
 }
 
-module.exports.getWhether=getWhether
+module.exports.getWheather=getWheather
+module.exports.getSortedTemp=getSortedTemp
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
 const wheatherController=require("../controllers/whetherController")
-
+const memeController=require("../controllers/memeController")
 
 
 router.get("/test-me", function (req, res) {
@@ -18,8 +18,13 @@ router.post("/cowin/getOtp", CowinController.getOtp)
 //router.get('/cowin/getByDistrct',CowinController.getByDistrict)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
+//assignement 1
 router.get('/cowin/getByDistrct',CowinController.getByDistrict)
-router.get('/getwheather',wheatherController.getWhether)
 
+//assigment 2
+router.get('/getwheather',wheatherController.getWheather)
+router.get('/getsortedtemp',wheatherController.getSortedTemp)
+
+//assigment 3
+router.post('/meme',memeController.memecreate)
 module.exports = router;
